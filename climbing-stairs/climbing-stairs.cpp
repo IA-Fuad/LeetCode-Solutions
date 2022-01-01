1325f23fff1815ae9858ba1c760d45c8f318efc3
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-01-01 20:55:48 +0600

    Time: 0 ms (100.00%), Space: 6.3 MB (13.67%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+    vector<int> dp;
+    int rec(int n) {
+        if (n < 0) return 0;
+        if (n == 0) return 1;
+        if (dp[n] != -1) return dp[n];
+        return dp[n] = rec(n-1) + rec(n-2);
+    }
+public:
+    int climbStairs(int n) {
+        dp.resize(n+1, -1);
+        return rec(n);
+    }
+};
