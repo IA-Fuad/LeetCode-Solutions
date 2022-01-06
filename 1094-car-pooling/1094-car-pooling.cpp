04b5518c5ca15de40db412189bbd59b15287c956
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-01-06 21:20:41 +0600

    Time: 17 ms (34.59%), Space: 10.1 MB (74.98%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        sort(trips.begin(), trips.end(), cmp);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pls;
+        // sort(trips.begin(), trips.end(), cmp);
+        // priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pls;
+        // for (auto &trip : trips) {
+        //     while(!pls.empty()) {
+        //         auto pl = pls.top();
+        //         if (pl.first <= trip[1]) {
+        //             capacity += pl.second;
+        //             pls.pop();
+        //         }
+        //         else break;
+        //     }
+        //     //cout << capacity << ' ' << trip[0] << endl;
+        //     if (capacity < trip[0]) return false;
+        //     capacity -= trip[0];
+        //     pls.push({trip[2], trip[0]});
+        // }
+        // return true;
+        map<int, int> stops;
         for (auto &trip : trips) {
-            while(!pls.empty()) {
-                auto pl = pls.top();
-                if (pl.first <= trip[1]) {
-                    capacity += pl.second;
-                    pls.pop();
-                }
-                else break;
-            }
-            //cout << capacity << ' ' << trip[0] << endl;
-            if (capacity < trip[0]) return false;
-            capacity -= trip[0];
-            pls.push({trip[2], trip[0]});
+            stops[trip[1]] += trip[0];
+            stops[trip[2]] -= trip[0];
+        }
+        for (auto &stop : stops) {
+            capacity -= stop.second;
+            if (capacity < 0) return false;
         }
         return true;
     }
