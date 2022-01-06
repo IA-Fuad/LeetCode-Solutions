eeccdf352c7f6038d6ec82d8a1fddb2126fd6a21
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-01-07 00:28:56 +0600

    Time: 20 ms (98.05%), Space: 23 MB (88.25%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -4,11 +4,9 @@ public:
         int mark[501] = {0};
         int cnt = 0;
         for (int t : time) {
-            int x = 60 - (t % 60);
-            for (int i = x; i <= 500; i += 60) {
-                cnt += mark[i];
-            }
-            mark[t]++;
+            int x = (60 - (t % 60)) % 60;
+            cnt += mark[x];
+            mark[t % 60]++;
         }
         return cnt;
     }
