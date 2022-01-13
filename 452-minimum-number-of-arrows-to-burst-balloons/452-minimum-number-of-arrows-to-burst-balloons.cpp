5ea9ea41a8d7066704b82884996806833447b118
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-01-13 22:45:44 +0600

    Time: 1079 ms (5.09%), Space: 89.8 MB (63.52%) - LeetHub

---
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        sort(points.begin(), points.end());
+        int end = points[0][1], arrows = 1;
+        cout << end << ' ';
+        for (int i = 1; i < points.size(); i++) {
+            if (end < points[i][0]) {
+                arrows++;
+                end = points[i][1];
+            }
+            else {
+                end = min(end, points[i][1]);
+            }
+            cout << end << ' ';
+        }
+        return arrows;
+    }
+};
