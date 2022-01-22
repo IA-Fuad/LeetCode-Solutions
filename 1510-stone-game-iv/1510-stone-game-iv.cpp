4a92c4afcb9864d17ebf2211e2ac129159d5bb31
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-01-22 20:22:53 +0600

    Time: 88 ms (49.33%), Space: 6.4 MB (61.74%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -3,23 +3,38 @@ class Solution {
     vector<int> isWinningComb;
     
     bool rec(int n, bool alice) {
-        if (n == 0) return false;
+        if (n == 0) return !alice;
         if (isWinningComb[n] != -1) return isWinningComb[n] == 1 ? true : false;
         
         bool win = false;
         for (int i = 0; i < squares.size(); i++) {
             if (squares[i] > n) break;
-            win |= (!rec(n - squares[i], !alice));
+            win |= (rec(n - squares[i], !alice));
         }
         //cout << n << ' ' << win << ' ' << move << endl;
-        return isWinningComb[n] = win;
+        return isWinningComb[n] = win && alice;
     }
     
 public:
     bool winnerSquareGame(int n) {
         for (int i = 1; i * i <= n; i++) squares.push_back(i * i);
-        isWinningComb.resize(n+1, -1);
+        //isWinningComb.resize(n+1, -1);
+        //return rec(n, true);
         
-        return rec(n, true);
+        vector <bool> dp(n+1);
+        dp[0] = false;
+        
+        for (int i = 1; i <= n; i++) {
+            bool win = false;
+            for (int k = 0; k < squares.size(); k++) {
+                if (squares[k] > i) break;
+                if (!dp[i - squares[k]]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[n];
     }
 };
