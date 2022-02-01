df5c3e538c0c4eea70a342a483974abe3dc698b4
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-02-02 00:06:49 +0600

    Time: 33 ms (26.16%), Space: 11.4 MB (45.93%) - LeetHub

--- 265-paint-house-ii/265-paint-house-ii.cpp
+++ 265-paint-house-ii/265-paint-house-ii.cpp
@@ -4,16 +4,25 @@ public:
         int n = costs.size(), K = costs[0].size();
         vector<int> colorCost(K, 0);
         
+        int mnCost = 0, secondMnCost = 0, mnColor = 0;
+        
         for (int i = 1; i <= n; i++) {
             auto currentColorCost = colorCost;
+            int currentMnCost = INT_MAX, currentSecondMnCost = INT_MAX, currentMnColor = 0;
             for (int k = 0; k < K; k++) {
-                int mnCost = INT_MAX;
-                for (int j = 0; j < K; j++) {
-                    if (j == k) continue;
-                    mnCost = min(mnCost, currentColorCost[j]);
+                if (mnColor == k) colorCost[k] = secondMnCost + costs[i-1][k];
+                else colorCost[k] = mnCost + costs[i-1][k];
+                
+                if (colorCost[k] < currentMnCost) {
+                    currentSecondMnCost = min(currentSecondMnCost, currentMnCost);
+                    currentMnCost = colorCost[k];
+                    currentMnColor = k;
                 }
-                colorCost[k] = mnCost + costs[i-1][k];
+                else if (colorCost[k] < currentSecondMnCost) {
+                    currentSecondMnCost = colorCost[k];
+                }
             }
+            mnCost = currentMnCost, secondMnCost = currentSecondMnCost, mnColor = currentMnColor;
         }
         
         return *min_element(colorCost.begin(), colorCost.end());
