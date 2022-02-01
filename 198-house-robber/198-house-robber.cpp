4ec71a17acf6b3f639d3dd9dc935becfb9975a9d
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-02-01 21:12:30 +0600

    Time: 3 ms (48.42%), Space: 7.6 MB (74.56%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -10,7 +10,19 @@ class Solution {
     
 public:
     int rob(vector<int>& nums) {
-        dp.resize(nums.size(), -1);
-        return rec(nums, 0);
+        // dp.resize(nums.size(), -1);
+        // return rec(nums, 0);
+        
+        if (nums.size() == 1) return nums[0];
+        
+        int prevPrev = nums[0], prev = max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.size(); i++) {
+            int temp = prev;
+            prev = max(prev, prevPrev + nums[i]);
+            prevPrev = temp;
+        }
+        
+        return prev;
     }
 };
