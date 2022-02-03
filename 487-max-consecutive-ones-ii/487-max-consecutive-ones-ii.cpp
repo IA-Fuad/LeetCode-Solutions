eb2b3475f21aa76866b7cdfa2d79d4ab7ac3e36b
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-02-04 00:54:55 +0600

    Time: 65 ms (31.00%), Space: 36.3 MB (12.64%) - LeetHub

--- 487-max-consecutive-ones-ii/487-max-consecutive-ones-ii.cpp
+++ 487-max-consecutive-ones-ii/487-max-consecutive-ones-ii.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> leftCount(n, 0), rightCount(n, 0);
-        
-        int maxConsecutiveOnes = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) continue;
-            else leftCount[i] = leftCount[max(0, i-1)] + 1;
-            maxConsecutiveOnes = max(maxConsecutiveOnes, leftCount[i]);
-        }
-
-        for (int i = n-1; i >= 0; i--) {
+        int zeroCount = 0, lastZeroInd = 0, mxOnes = 0;
+        for (int i = 0, l = 0; i < nums.size(); i++) {
             if (nums[i] == 0) {
-                int consecutiveOnes = leftCount[max(0, i-1)] + rightCount[min(n-1, i+1)] + 1;
-                maxConsecutiveOnes = max(maxConsecutiveOnes, consecutiveOnes);
+                zeroCount++;
+                if (zeroCount > 1) {
+                    zeroCount--;
+                    l = lastZeroInd + 1;
+                }
+                lastZeroInd = i;
             }
-            else rightCount[i] = rightCount[min(n-1, i+1)] + 1;
+            mxOnes = max(mxOnes, i - l + 1);
         }
         
-        return maxConsecutiveOnes;
+        return mxOnes;
     }
 };
