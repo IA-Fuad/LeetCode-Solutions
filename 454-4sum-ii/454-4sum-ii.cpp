e7f7ddc0b336c7b5f38aec6f924a807433750c41
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-02-03 15:15:44 +0600

    Time: 421 ms (24.75%), Space: 25.2 MB (17.98%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -1,22 +1,30 @@
 class Solution {
+    //unordered_map<int, int> kSumCount;
+    
 public:
-    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
-        unordered_map<int, int> pairSum;
-        for (int n1 : nums1) {
-            for (int n2 : nums2) {
-                pairSum[n1+n2]++;
-            }
-        }
-        
-        int tupleCount = 0;
-        
-        for (int n3 : nums3) {
-            for (int n4 : nums4) {
-                int numberNeedToMakeZero = -(n3 + n4);
-                tupleCount += pairSum[numberNeedToMakeZero];
-            }
-        }
-        
-        return tupleCount;
+    int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+    return kSumCount(vector<vector<int>>() = {A, B, C, D});
+}
+int kSumCount(vector<vector<int>> &lists) {
+    unordered_map<int, int> m;
+    addToHash(lists, m, 0, 0);
+    return countComplements(lists, m, lists.size() / 2, 0);
+}
+void addToHash(vector<vector<int>> &lists, unordered_map<int, int> &m, int i, int sum) {
+    if (i == lists.size() / 2)
+        ++m[sum];
+    else
+        for (int a : lists[i])
+            addToHash(lists, m, i + 1, sum + a);
+}
+int countComplements(vector<vector<int>> &lists, unordered_map<int, int> &m, int i, int complement) {
+    if (i == lists.size()) {
+        auto it = m.find(complement);
+        return it == end(m) ? 0 : it->second;
     }
+    int cnt = 0;
+    for (int a : lists[i])
+        cnt += countComplements(lists, m, i + 1, complement - a);
+    return cnt;
+}
 };
