b00c4780576f0c1749f5d499f772a4a0ddbe0af1
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-02-05 17:02:49 +0600

    Time: 330 ms (14.40%), Space: 348.8 MB (5.03%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -29,16 +29,39 @@ class Solution {
         return head;
     }
     
+    ListNode* merge(vector<ListNode*> lists, int s, int e) {
+        if (s == e) return lists[s];
+        
+        int mid = s + (e - s) / 2;
+        
+        auto left = merge(lists, s, mid);
+        auto right = merge(lists, mid+1, e);
+        
+        return mergeList(left, right);
+    }
+    
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if (lists.size() == 0) return nullptr;
 
-        ListNode* head = lists[0];
+//         ListNode* head = lists[0];
         
-        for (int i = 1; i < lists.size(); i++) {
-            head = mergeList(head, lists[i]);
-        }
+//         for (int i = 1; i < lists.size(); i++) {
+//             head = mergeList(head, lists[i]);
+//         }
         
-        return head;
+//         return head;
+        
+         int k = lists.size(), interval = 1;
+//         while (interval < k) {
+//             for (int i = 0; i+interval < k; i += interval * 2) {
+//                 lists[i] = mergeList(lists[i], lists[i+interval]);
+//             }
+//             interval *= 2;
+//         }
+        
+//         return lists[0];
+        
+        return merge(lists, 0, k-1);
     }
 };
