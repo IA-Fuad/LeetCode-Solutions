d755a25a9b92536fd31443b72eb48523656cd204
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-02-08 00:46:15 +0600

    Time: 104 ms (60.39%), Space: 13.3 MB (91.39%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -13,27 +13,52 @@ class Solution {
         return i == a.size();
     }
     
+    vector<int> dp;
+    
+    int rec(vector<string>& words, int i) {
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+        int mxLen = 1;
+        for (int j = i-1; j >= 0; j--) {
+            if (words[j].size() < words[i].size()-1) {
+                break;
+            }
+            if (isPredecessor(words[j], words[i])) {
+                mxLen = max(mxLen, rec(words, j) + 1);
+                //cout << words[j] << ' ' << words[i] << endl;
+            }
+        }
+        return dp[i] = mxLen;
+    }
+    
 public:
     int longestStrChain(vector<string>& words) {
-        int n = words.size();
-        sort(words.begin(), words.end(), cmp);
-        vector<int> longestChains(n, 1);
+//         int n = words.size();
+//         sort(words.begin(), words.end(), cmp);
+//         vector<int> longestChains(n, 1);
 
-        int longestChain = 1;
+//         int longestChain = 1;
         
-        for (int i = 1; i < n; i++) {
-            for (int j = i-1; j >= 0; j--) {
-                if (words[j].size() < words[i].size()-1) {
-                    break;
-                }
-                if (isPredecessor(words[j], words[i]) && longestChains[j] + 1 > longestChains[i]) {
-                    longestChains[i] = longestChains[j] + 1;
-                }
-            }
-            longestChain = max(longestChain, longestChains[i]);
-        }
+//         for (int i = 1; i < n; i++) {
+//             for (int j = i-1; j >= 0; j--) {
+//                 if (words[j].size() < words[i].size()-1) {
+//                     break;
+//                 }
+//                 if (isPredecessor(words[j], words[i]) && longestChains[j] + 1 > longestChains[i]) {
+//                     longestChains[i] = longestChains[j] + 1;
+//                 }
+//             }
+//             longestChain = max(longestChain, longestChains[i]);
+//         }
         
-        return longestChain;
+//         return longestChain;
+        
+        int n = words.size(), mxLen = 0;
+        dp.resize(n, -1);
+        sort(words.begin(), words.end(), cmp);
+        for (int i = n-1; i >= 0; i--) mxLen = max(mxLen, rec(words, i));
+        return mxLen;
     }
     
     bool static cmp(string& x, string& y) {
