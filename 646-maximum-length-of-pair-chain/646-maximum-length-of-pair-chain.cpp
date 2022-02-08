bde22ceae797aa197f96ac37e7c899e2d93d6783
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-02-08 22:42:33 +0600

    Time: 74 ms (84.08%), Space: 22.5 MB (88.69%) - LeetHub

--- 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.cpp
@@ -2,19 +2,31 @@ class Solution {
 public:
     int findLongestChain(vector<vector<int>>& pairs) {
         int n = pairs.size();
-        sort(pairs.begin(), pairs.end());
-        vector<int> chainLengths(n, 1);
-        int longestChain = 1;
+        sort(pairs.begin(), pairs.end(), cmp);
+//         vector<int> chainLengths(n, 1);
+//         int longestChain = 1;
         
+//         for (int i = 1; i < n; i++) {
+//             for (int j = i-1; j >= 0; j--) {
+//                 if (pairs[j][1] < pairs[i][0] && chainLengths[j]+1 > chainLengths[i]) {
+//                     chainLengths[i] = chainLengths[j] + 1;
+//                 }
+//             }
+//             longestChain = max(longestChain, chainLengths[i]);
+//         }
+        
+//         return longestChain;
+        int longestChain = 1, lastRight = pairs[0][1];
         for (int i = 1; i < n; i++) {
-            for (int j = i-1; j >= 0; j--) {
-                if (pairs[j][1] < pairs[i][0] && chainLengths[j]+1 > chainLengths[i]) {
-                    chainLengths[i] = chainLengths[j] + 1;
-                }
+            if (pairs[i][0] > lastRight) {
+                lastRight = pairs[i][1];
+                longestChain++;
             }
-            longestChain = max(longestChain, chainLengths[i]);
         }
-        
         return longestChain;
+    }
+    
+    bool static cmp(vector<int>& x, vector<int>& y) {
+        return x[1] < y[1];
     }
 };
