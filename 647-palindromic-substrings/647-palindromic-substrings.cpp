e0c0a70acc6b2baafdd41982ed7ea6cd7ae4107e
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-02-10 00:04:12 +0600

    Time: 7 ms (72.90%), Space: 6.1 MB (96.60%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,30 +1,42 @@
-class Solution {   
-    void rec(int l, int r) {
-        for (int i = l; i <= r; i++) {}
+class Solution {  
+    int getPalindromeCountAroundCenter(const string& s, int l, int r) {
+        int count = 0;
+        while (l >= 0 && r < s.size()) {
+            if (s[l] != s[r]) {
+                break;
+            }
+            l--;
+            r++;
+            count++;
+        }
+        return count;
     }
-    
     
 public:
     int countSubstrings(string s) {
         int n = s.size();
-        vector<bool> isPalindrome(n, false);
+        //vector<bool> isPalindrome(n, false);
         int palindromeCount = 0;
  
-        for (int j = 0; j < n; j++) {
-            for (int i = 0; i <= j; i++) {
-                if (i == j) {
-                    isPalindrome[i] = true;
-                }
-                else if (j == i+1) {
-                    isPalindrome[i] = s[i] == s[j];
-                }
-                else {
-                    isPalindrome[i] = isPalindrome[i+1] & s[i] == s[j];
-                }
-                if (isPalindrome[i]) {
-                    palindromeCount++;
-                }
-            }
+        // for (int j = 0; j < n; j++) {
+        //     for (int i = 0; i <= j; i++) {
+        //         if (i == j) {
+        //             isPalindrome[i] = true;
+        //         }
+        //         else if (j == i+1) {
+        //             isPalindrome[i] = s[i] == s[j];
+        //         }
+        //         else {
+        //             isPalindrome[i] = isPalindrome[i+1] & s[i] == s[j];
+        //         }
+        //         if (isPalindrome[i]) {
+        //             palindromeCount++;
+        //         }
+        //     }
+        // }
+        for (int i = 0; i < n; i++) {
+            palindromeCount += getPalindromeCountAroundCenter(s, i, i);
+            palindromeCount += getPalindromeCountAroundCenter(s, i, i+1);
         }
         
         return palindromeCount;
