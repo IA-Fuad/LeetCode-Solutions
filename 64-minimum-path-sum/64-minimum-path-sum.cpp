773a8104e062d18ab1817bef52ba660e9e0d264d
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-02-12 01:07:09 +0600

    Time: 15 ms (39.32%), Space: 9.7 MB (86.52%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -8,9 +8,7 @@ public:
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 pathSum[j] = min(pathSum[j-1], pathSum[j]) + grid[i-1][j-1]; 
-                cout << pathSum[j] << ' ';
             }
-            cout << endl;
         }
         
         return pathSum[n];
