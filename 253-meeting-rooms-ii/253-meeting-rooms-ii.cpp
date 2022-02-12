8f163a453c60ad3b907f5cc0fb488dfb9d3dae06
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-02-12 19:00:35 +0600

    Time: 14 ms (93.08%), Space: 11.7 MB (97.76%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
@@ -2,20 +2,16 @@ class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
-        priority_queue<int, vector<int>, greater<int>> endTime;
-        int roomNeeded = 1;
-        endTime.push(intervals[0][1]);
+        priority_queue<int, vector<int>, greater<int>> rooms;
+        rooms.push(intervals[0][1]);
         
         for (int i = 1; i < intervals.size(); i++) {
-            if (intervals[i][0] < endTime.top()) {
-                roomNeeded++;
+            if (intervals[i][0] >= rooms.top()) {
+                rooms.pop();
             }
-            else {
-                endTime.pop();
-            }
-            endTime.push(intervals[i][1]);
+            rooms.push(intervals[i][1]);
         }
         
-        return roomNeeded;
+        return rooms.size();
     }
 };
