76554152a06f16fbd528e2a75fea787fe55ba7e5
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-02-13 23:12:25 +0600

    Time: 79 ms (45.79%), Space: 17.7 MB (30.70%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,4 +1,7 @@
 class Solution {
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    
     int m, n, longestIncreasingPathLen;
     vector<vector<int>> longestIncreasingLen;
     
@@ -30,18 +33,72 @@ class Solution {
 public:
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         m = matrix.size(), n = matrix[0].size();
-        longestIncreasingLen.resize(m, vector<int>(n, -1));
+//         longestIncreasingLen.resize(m, vector<int>(n, -1));
         
-        auto increasingComparator = [](int x, int y) -> int {
-            return x > y;
-        };
-        auto decreasingCompartor = [](int x, int y) -> int {
-            return x < y;
-        };
+//         auto increasingComparator = [](int x, int y) -> int {
+//             return x > y;
+//         };
+//         auto decreasingCompartor = [](int x, int y) -> int {
+//             return x < y;
+//         };
+        
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 longestIncreasingPathLen = max(longestIncreasingPathLen, rec(matrix, i, j, increasingComparator));
+//             }
+//         }
+        
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 cout << longestIncreasingLen[i][j] << ' ';
+//             }
+//             cout << endl;
+//         }
+        vector<vector<int>> outDegree(m, vector<int>(n, 0));
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                longestIncreasingPathLen = max(longestIncreasingPathLen, rec(matrix, i, j, increasingComparator));
+                for (int k = 0; k < 4; k++) {
+                    int x = i + dx[k];
+                    int y = j + dy[k];
+                    if (x < 0 || y < 0 || x == m || y == n) continue;
+                    outDegree[i][j] += (matrix[x][y] > matrix[i][j]);
+                }
+            }
+        }
+        
+        deque<pair<int, int>> leaves;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (outDegree[i][j] == 0) {
+                    leaves.push_back({i, j});
+                }
+            }
+        }
+        
+        while (!leaves.empty()) {
+            longestIncreasingPathLen++;
+            int currentLeaves = leaves.size();
+            
+            while (currentLeaves > 0) {
+                int i = leaves.front().first;
+                int j = leaves.front().second;
+                leaves.pop_front();
+                currentLeaves--;
+                
+                for (int k = 0; k < 4; k++) {
+                    int x = i + dx[k];
+                    int y = j + dy[k];
+                    if (x < 0 || y < 0 || x == m || y == n) continue;
+
+                    if (matrix[x][y] < matrix[i][j]) {
+                        outDegree[x][y]--;
+                        if (outDegree[x][y] == 0) {
+                            leaves.push_back({x, y});
+                        }
+                    }
+                }
             }
         }
         
