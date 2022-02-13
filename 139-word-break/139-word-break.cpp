10f0c0a02036e7f191617fc0f8e9ebbbb2bea622
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-02-13 12:15:27 +0600

    Time: 18 ms (46.72%), Space: 13.2 MB (44.18%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.size();
-        vector<vector<bool>> isWord(n+1, vector<bool>(n+1, false));
+        vector<bool> isWord(n+1);
         unordered_map<string, bool> words;
         for (string& w : wordDict) {
             words[w] = true;
@@ -11,12 +11,12 @@ public:
         for (int i = 1; i <= n; i++) {
             bool validWord = words.find(s.substr(0, i)) != words.end();
             for (int k = 1; k < i && !validWord; k++) {
-                validWord = isWord[1][k] && (words.find(s.substr(k, i-k)) != words.end());
+                validWord = isWord[k] && (words.find(s.substr(k, i-k)) != words.end());
                 //cout << s.substr(k, i-k) << ' ';
             }
-            isWord[1][i] = validWord;
+            isWord[i] = validWord;
         }
         
-        return isWord[1][n];
+        return isWord[n];
     }
 };
