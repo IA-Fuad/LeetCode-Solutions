834b5812b902593663bf88f5ebfb3a85d541e279
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-02-15 11:33:12 +0600

    Time: 25 ms (30.27%), Space: 9.4 MB (42.28%) - LeetHub

---
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    const int mod = 1e9 + 7;
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    
+    int rec(vector<vector<vector<int>>>& dp, int i, int j, int m, int n, int move) {
+        if (i < 0 || j < 0 || i == m || j == n) return 1;
+        if (move == 0) return 0;
+        if (dp[i][j][move] != -1) return dp[i][j][move];
+        
+        int paths = 0;
+        for (int k = 0; k < 4; k++) {
+            int p = rec(dp, i+dx[k], j+dy[k], m, n, move-1);
+            paths = (paths + p) % mod;
+        }
+        
+        return dp[i][j][move] = paths % mod;
+    }
+    
+public:
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(maxMove+1, -1)));
+        return rec(dp, startRow, startColumn, m, n, maxMove);
+    }
+};
