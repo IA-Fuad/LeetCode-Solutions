f999720793da584fe3e82b2b2ec3965ef1b7c2e4
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-02-19 20:55:19 +0600

    Time: 422 ms (19.90%), Space: 12 MB (53.44%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -3,10 +3,7 @@ class Solution {
         if (target == 0) return true;
         if (target < 0 || i == nums.size()) return false;
         if (dp[i][target] != -1) return dp[i][target];
-        // bool can = false;
-        // for (int k = i; k < nums.size(); k++) {
-        //     can |= rec(nums, k+1, target-nums[k], dp);
-        // }
+
         return dp[i][target] = rec(nums, i+1, target, dp) | rec(nums, i+1, target-nums[i], dp);
     }
     
@@ -17,11 +14,17 @@ public:
         if (n == 1 || totalSum&1) return false;
         
         int half = totalSum / 2;
-        vector<vector<int>> dp(n+1, vector<int>(half+1, -1));
-        return rec(nums, 0, half, dp);
+        vector<vector<bool>> dp(n+1, vector<bool>(half+1, false));
+        //return rec(nums, 0, half, dp);
         
-//         
+        for (int i = 1; i <= n; i++) {
+            dp[i-1][0] = true;
+            for (int t = 1; t <= half; t++) {
+                if (nums[i-1] > t) dp[i][t] = dp[i-1][t];
+                else dp[i][t] = dp[i-1][t] | dp[i-1][t-nums[i-1]];
+            }
+        }
         
-//         for (int i = 0)
+        return dp[n][half];
     }
 };
