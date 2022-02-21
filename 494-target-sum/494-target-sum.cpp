73a125d2cb22fd63b99b484cc28e8e27f1288442
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-02-21 14:16:29 +0600

    Time: 174 ms (38.90%), Space: 22.7 MB (28.33%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -18,7 +18,31 @@ class Solution {
     
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        unordered_map<string, int> dp;
-        return rec(nums, target, dp, 0, 0);
+        // unordered_map<string, int> dp;
+        // return rec(nums, target, dp, 0, 0);
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        if (target < -total or target > total) return 0;
+        
+        int n = nums.size();
+
+        vector<vector<int>> dp(n+1, vector<int>(total*2+1));
+        
+        dp[n][total] = 1;
+
+        for (int k = n-1; k >= 0; k--) {
+            for (int s = -total; s <= total; s++) {
+                int shiftedS = s + total;
+                int positive = shiftedS + nums[k];
+                int negative = shiftedS - nums[k];
+                if (positive >= 0 and positive <= 2*total) {
+                    dp[k][shiftedS] += dp[k+1][shiftedS+nums[k]];
+                }
+                if (negative >= 0 and negative <= 2*total) {
+                    dp[k][shiftedS] += dp[k+1][shiftedS-nums[k]];
+                }
+            }
+        }
+
+        return dp[0][target+total];
     }
 };
