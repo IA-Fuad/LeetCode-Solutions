65ef9cf4785e2351c272a3bd60cd564bd18a405a
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-02-22 16:54:13 +0600

    Time: 467 ms (16.36%), Space: 9.2 MB (86.28%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -6,18 +6,20 @@ public:
         
         int target = totalSum / 2;
         
-        vector<vector<bool>> dp(n+1, vector<bool>(target+1, false));
+        //vector<bool> c(target+1, false), p(target+1, false);
+        vector<bool>* currRow = new vector<bool>(target+1, false);
+        vector<bool>* prevRow = new vector<bool>(target+1, false);
         
-        dp[0][0] = true;
+        (*prevRow)[0] = true;
         for (int i = 1; i <= n; i++) {
             for (int t = 1; t <= target; t++) {
-                dp[i][t] = dp[i-1][t];
+                (*currRow)[t] = (*prevRow)[t];
                 int need = t - nums[i-1];
                 if (need < 0) continue;
-                dp[i][t] = dp[i][t] | dp[i-1][need];
+                (*currRow)[t] = (*currRow)[t] | (*prevRow)[need];
             }
+            swap(currRow, prevRow);
         }
-        
-        return dp[n][target];
+        return (*prevRow)[target];
     }
 };
