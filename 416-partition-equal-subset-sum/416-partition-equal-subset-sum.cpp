1ac50b5b9b62a2ab8d541fb5f93ff3464afaa178
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-02-22 16:40:43 +0600

    Time: 432 ms (19.24%), Space: 12.1 MB (47.40%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,30 +1,23 @@
-class Solution {
-    bool rec(vector<int>& nums, int i, int target, vector<vector<int>>& dp) {
-        if (target == 0) return true;
-        if (target < 0 || i == nums.size()) return false;
-        if (dp[i][target] != -1) return dp[i][target];
-
-        return dp[i][target] = rec(nums, i+1, target, dp) | rec(nums, i+1, target-nums[i], dp);
-    }
-    
+class Solution {    
 public:
     bool canPartition(vector<int>& nums) {
-        int totalSum = accumulate(nums.begin(), nums.end(), 0);
-        int n = nums.size();
-        if (n == 1 || totalSum&1) return false;
+        int n = nums.size(), totalSum = accumulate(nums.begin(), nums.end(), 0);
+        if (totalSum&1) return false;
         
-        int half = totalSum / 2;
-        vector<vector<bool>> dp(n+1, vector<bool>(half+1, false));
-        //return rec(nums, 0, half, dp);
+        int target = totalSum / 2;
         
+        vector<vector<bool>> dp(n+1, vector<bool>(target+1, false));
+        
+        dp[0][0] = true;
         for (int i = 1; i <= n; i++) {
-            dp[i-1][0] = true;
-            for (int t = 1; t <= half; t++) {
-                if (nums[i-1] > t) dp[i][t] = dp[i-1][t];
-                else dp[i][t] = dp[i-1][t] | dp[i-1][t-nums[i-1]];
+            for (int t = 1; t <= target; t++) {
+                dp[i][t] = dp[i-1][t];
+                int need = t - nums[i-1];
+                if (need < 0) continue;
+                dp[i][t] = dp[i][t] | dp[i-1][need];
             }
         }
         
-        return dp[n][half];
+        return dp[n][target];
     }
 };
