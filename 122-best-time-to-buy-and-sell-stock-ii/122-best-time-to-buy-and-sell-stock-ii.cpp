08487ec53986bb813d025a1dba2d95dc258e59a6
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-02-23 10:42:03 +0600

    Time: 4 ms (91.69%), Space: 15.7 MB (5.59%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,25 +1,25 @@
 class Solution {
-    int rec(vector<int>& prices, int i, bool canBuy, bool canSell, int (*dp)[2][2]) {
+    int rec(vector<int>& prices, int i, bool canBuy, int (*dp)[2]) {
         if (i == prices.size()) return 0;
-        if (dp[i][canBuy][canSell] != -1) return dp[i][canBuy][canSell];
+        if (dp[i][canBuy] != -1) return dp[i][canBuy];
         
         int mx = INT_MIN;
         if (canBuy) {
-            mx = rec(prices, i + 1, false, true, dp) - prices[i];
+            mx = rec(prices, i + 1, false, dp) - prices[i];
         }
-        if (canSell) {
-            mx = rec(prices, i + 1, true, false, dp) + prices[i];
+        else {
+            mx = rec(prices, i + 1, true, dp) + prices[i];
         }
-        mx = max(mx, rec(prices, i + 1, canBuy, canSell, dp));
+        mx = max(mx, rec(prices, i + 1, canBuy, dp));
         
-        return dp[i][canBuy][canSell] = mx;
+        return dp[i][canBuy] = mx;
     }
     
 public:
     int maxProfit(vector<int>& prices) {
-        int dp[prices.size()][2][2];
+        int dp[prices.size()][2];
         memset(dp, -1, sizeof dp);
         
-        return rec(prices, 0, true, false, dp);
+        return rec(prices, 0, true, dp);
     }
 };
