2ea8ce3a44277cdc4594d5b90a2901311b372374
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-02-26 20:29:29 +0600

    Time: 126 ms (40.76%), Space: 10.9 MB (77.89%) - LeetHub

--- 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
@@ -1,16 +1,15 @@
 class Solution {
+    int endMask;
     vector<vector<int>> dp;
     int rec(vector<vector<int>>& graph, int i, int mask) {
-        if (mask & (mask-1) == 0) return 0;
-        if (dp[i][mask] > -1) return dp[i][mask];
+        if (mask == endMask) return 0;
+        if (dp[i][mask] != -1) return dp[i][mask];
         
-        int mn = INT_MAX-1;
+        dp[i][mask] = INT_MAX - 1;
         for (int& node : graph[i]) {
-            if (dp[node][(mask^(1<<node))] == -2) continue;
-            dp[node][(mask^(1<<node))] = -2;
-            mn = min(mn, 1 + rec(graph, node, mask ^ (1 << node)));
+            dp[i][mask] = min(dp[i][mask], 1 + min(rec(graph, node, mask), rec(graph, node, mask | (1 << node))));
         }
-        return dp[i][mask] = mn;
+        return dp[i][mask];
     }
     
     struct State {
@@ -21,40 +20,41 @@ class Solution {
     
 public:
     int shortestPathLength(vector<vector<int>>& graph) {
-        // int n = graph.size();
-        // int mask = (1 << n) - 1;
-        // dp.resize(n, vector<int>(mask+1, -1));
-        // int mn = INT_MAX;
-        // for (int i = 0; i < n; i++) {
-        //     mn = min(mn, rec(graph, i, mask ^ (1 << i)));
-        // }
-        // return mn;
-        
         int n = graph.size();
-        int endMask = (1 << n) - 1;
-        vector<vector<bool>> seen(n, vector<bool>(endMask+1, false));
-
-        queue<State> Q;
+        endMask = (1 << n) - 1;
+        int mask = (1 << n) - 1;
+        dp.resize(n, vector<int>(endMask+1, -1));
+        int mn = INT_MAX;
         for (int i = 0; i < n; i++) {
-            Q.push({i, 1 << i, 0});
-            seen[i][(1 << i)] = true;
+            mn = min(mn, rec(graph, i, (1 << i)));
         }
-
-        while (!Q.empty()) {
-            auto currState = Q.front();
-            Q.pop();
-            
-            for (int k : graph[currState.node]) {
-                int currMask = currState.mask | (1 << k);
-
-                if (!seen[k][currMask]) {
-                    if (currMask == endMask) return currState.steps + 1;
-                    seen[k][currMask] = true;
-                    Q.push({k, currMask, currState.steps + 1});
-                }
-            }
-        }
+        return mn;
         
-        return 0;
+//         int n = graph.size();
+//         int endMask = (1 << n) - 1;
+//         vector<vector<bool>> seen(n, vector<bool>(endMask+1, false));
+
+//         queue<State> Q;
+//         for (int i = 0; i < n; i++) {
+//             Q.push({i, 1 << i, 0});
+//             seen[i][(1 << i)] = true;
+//         }
+
+//         while (!Q.empty()) {
+//             auto currState = Q.front();
+//             Q.pop();
+            
+//             for (int k : graph[currState.node]) {
+//                 int currMask = currState.mask | (1 << k);
+
+//                 if (!seen[k][currMask]) {
+//                     if (currMask == endMask) return currState.steps + 1;
+//                     seen[k][currMask] = true;
+//                     Q.push({k, currMask, currState.steps + 1});
+//                 }
+//             }
+//         }
+        
+//         return 0;
     }
 };
