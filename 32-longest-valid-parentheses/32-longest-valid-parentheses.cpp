a256b5ce2d6418df19a07136861e4158dd981c1c
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-03-03 00:20:58 +0600

    Time: 3 ms (88.81%), Space: 7.3 MB (41.20%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        vector<int> dp(s.size(), 0);
-        int maxLen = 0;
+//         vector<int> dp(s.size(), 0);
+//         int maxLen = 0;
         
-        for (int i = 1; i < s.size(); i++) {
-            if (s[i] == ')') {
-                if (s[i-1] == '(') {
-                    dp[i] = (i < 2 ? 0 : dp[i-2]) + 2;
+//         for (int i = 1; i < s.size(); i++) {
+//             if (s[i] == ')') {
+//                 if (s[i-1] == '(') {
+//                     dp[i] = (i < 2 ? 0 : dp[i-2]) + 2;
+//                 }
+//                 else if (i - dp[i-1] - 1 >= 0 and s[i - dp[i-1] - 1] == '(') {
+//                     dp[i] = dp[i-1] + (i - dp[i-1] - 2 < 0 ? 0 : dp[i - dp[i-1] - 2]) + 2;
+//                 }
+//                 maxLen = max(maxLen, dp[i]);
+//             }
+//         }
+        
+        int maxLen = 0, leftMostInd = -1;
+        stack<int> S;
+        S.push(-1);
+        
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                S.push(i);
+            }
+            else if (!S.empty()) {
+                S.pop();
+                if (!S.empty()) {
+                    maxLen = max(maxLen, i - S.top());
                 }
-                else if (i - dp[i-1] - 1 >= 0 and s[i - dp[i-1] - 1] == '(') {
-                    dp[i] = dp[i-1] + (i - dp[i-1] - 2 < 0 ? 0 : dp[i - dp[i-1] - 2]) + 2;
-                }
-                maxLen = max(maxLen, dp[i]);
+            }
+            if (s[i] == ')' and S.empty()) {
+                S.push(i);
             }
         }
         
