6ae8f2eb670cc8088e42fb8033c8d5d6afc01449
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-03-02 13:36:28 +0600

    Time: 327 ms (61.39%), Space: 64.6 MB (47.94%) - LeetHub

--- 815-bus-routes/815-bus-routes.cpp
+++ 815-bus-routes/815-bus-routes.cpp
@@ -30,16 +30,29 @@ class Solution {
     }
     
 public:
-    int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
-        if (source == target) return 0;
-        unordered_map<int, vector<int>> routeToBus;
-        
-        for (int b = 0; b < routes.size(); b++) {
-            for (int r : routes[b]) {
-                routeToBus[r].push_back(b);
+    int numBusesToDestination(vector<vector<int>>& routes, int S, int T) {
+        unordered_map<int, vector<int>> to_routes;
+        for (int i = 0; i < routes.size(); ++i)
+            for (int j : routes[i])
+                to_routes[j].push_back(i);
+        queue<pair<int, int>> bfs;
+        bfs.push({S, 0});
+        unordered_set<int> seen = {S};
+        while (!bfs.empty()) {
+            int stop = bfs.front().first, bus = bfs.front().second;
+            bfs.pop();
+            if (stop == T)
+                return bus;
+            for (int i : to_routes[stop]) {
+                for (int j : routes[i]) {
+                    if (seen.find(j) == seen.end()) {
+                        seen.insert(j);
+                        bfs.push({j, bus + 1});
+                    }
+                }
+                routes[i].clear();
             }
         }
-        
-        return getNumberOfBusesToDest(routeToBus, routes, source, target);
+        return -1;
     }
 };
