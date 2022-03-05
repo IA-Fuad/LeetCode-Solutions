7f63851b93ee5933862a44a7f4b54a9da2ed7f65
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-03-05 23:37:19 +0600

    Time: 5 ms (81.02%), Space: 8.8 MB (84.39%) - LeetHub

--- 174-dungeon-game/174-dungeon-game.cpp
+++ 174-dungeon-game/174-dungeon-game.cpp
@@ -2,26 +2,28 @@ class Solution {
 public:
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
         int m = dungeon.size(), n = dungeon[0].size();
-        vector<vector<int>> hp(m+1, vector<int>(n+1, INT_MAX));
+        vector<int> hp(n+1, INT_MAX);
+        vector<int> nextRowHp(n+1, INT_MAX);
 
         for (int i = m-1; i >= 0; i--) {
             for (int j = n-1; j >= 0; j--) {
                 if (i == m-1 and j == n-1) {
-                    hp[i][j] = dungeon[i][j] > 0 ? 1 : (-dungeon[i][j] + 1);
+                    hp[j] = dungeon[i][j] > 0 ? 1 : (-dungeon[i][j] + 1);
                     continue;
                 }
                 
-                int minAdj = min(hp[i][j+1], hp[i+1][j]);
+                int minAdj = min(hp[j+1], nextRowHp[j]);
                 if (dungeon[i][j] > 0) {
-                    hp[i][j] = max(1, minAdj - dungeon[i][j]);
+                    hp[j] = max(1, minAdj - dungeon[i][j]);
                 }
                 else {
-                    hp[i][j] = -dungeon[i][j] + minAdj;
+                    hp[j] = -dungeon[i][j] + minAdj;
                 }
             }
+            nextRowHp = hp;
         }
         
-        return hp[0][0];
+        return hp[0];
     }
 };
 
