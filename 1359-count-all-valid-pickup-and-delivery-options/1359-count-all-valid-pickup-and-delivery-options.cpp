c19c8ea917499194da69426ed16ed184e1dc44a3
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-03-06 13:11:02 +0600

    Time: 0 ms (100.00%), Space: 6 MB (53.14%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -18,26 +18,31 @@ class Solution {
 public:
     int countOrders(int n) {
         // vector<vector<int>> alreadyCounted(n+1, vector<int>(n+1, -1));
-        // return countOrders(n, n, alreadyCounted);
-        
-        
-        vector<vector<int>> dp(n+1, vector<int>(n+1));
-
-        for (int unPicked = 0; unPicked <= n; unPicked++) {
-             for (int unDelivered = unPicked; unDelivered <= n; unDelivered++) {
-                if (unPicked == 0 and unDelivered == 0) {
-                    dp[0][0] = 1;
-                    continue;
-                } 
+        // return countOrders(n, n, alreadyCounted); 
+//         for (int unPicked = 0; unPicked <= n; unPicked++) {
+//              for (int unDelivered = unPicked; unDelivered <= n; unDelivered++) {
+//                 if (unPicked == 0 and unDelivered == 0) {
+//                     dp[0][0] = 1;
+//                     continue;
+//                 } 
                 
-                int64_t waysToPick = unPicked > 0 ? ((int64_t)unPicked * dp[unPicked-1][unDelivered]) % MOD : 0;
-                int64_t waysToDeliver = unDelivered > unPicked ? ((int64_t)(unDelivered - unPicked) * dp[unPicked][unDelivered-1]) % MOD : 0;
+//                 int64_t waysToPick = unPicked > 0 ? ((int64_t)unPicked * dp[unPicked-1][unDelivered]) % MOD : 0;
+//                 int64_t waysToDeliver = unDelivered > unPicked ? ((int64_t)(unDelivered - unPicked) * dp[unPicked][unDelivered-1]) % MOD : 0;
 
-                dp[unPicked][unDelivered] = (waysToPick + waysToDeliver) % MOD;
-            }
+//                 dp[unPicked][unDelivered] = (waysToPick + waysToDeliver) % MOD;
+//             }
+//         }
+        
+//         return dp[n][n];
+        
+        int64_t ans = 1;
+        
+        for (int i = 1; i <= n; i++) {
+            ans = (ans * i) % MOD;
+            ans = (ans * ((i * 2) - 1)) % MOD;
         }
         
-        return dp[n][n];
+        return ans;
     }
 };
 
