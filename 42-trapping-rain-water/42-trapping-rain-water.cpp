c1d40002b5e40317115bc3c32c4f55f0e84c1fa7
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-03-08 01:02:30 +0600

    Time: 21 ms (28.92%), Space: 17.8 MB (29.95%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,23 +1,42 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        stack<int> bars;
+//         stack<int> bars;
+//         int ans = 0;
+//         for (int i = 0; i < height.size(); i++) {
+//             while (!bars.empty() and height[i] > height[bars.top()]) {
+//                 int middleBar = bars.top();
+//                 bars.pop();
+                
+//                 if (bars.empty()) break;
+                
+//                 int leftBar = bars.top();
+//                 int rightBar = i;
+//                 int boundedHeight = min(height[leftBar], height[rightBar]) - height[middleBar];
+//                 int distance = rightBar - leftBar - 1;
+
+//                 ans += (distance * boundedHeight);
+//             }
+//             bars.push(i);
+//         }
+        
+//         return ans;
+        
         int ans = 0;
-        for (int i = 0; i < height.size(); i++) {
-            while (!bars.empty() and height[i] > height[bars.top()]) {
-                int middleBar = bars.top();
-                bars.pop();
-                
-                if (bars.empty()) break;
-                
-                int leftBar = bars.top();
-                int rightBar = i;
-                int boundedHeight = min(height[leftBar], height[rightBar]) - height[middleBar];
-                int distance = rightBar - leftBar - 1;
-                
-                ans += (distance * boundedHeight);
+        int left = 0, right = height.size() - 1, leftMax = 0, rightMax = 0;
+        
+        while (left < right) {
+            leftMax = max(leftMax, height[left]);
+            rightMax = max(rightMax, height[right]);
+            
+            if (leftMax > rightMax) {
+                ans += (rightMax - height[right]);
+                right--;
             }
-            bars.push(i);
+            else {
+                ans += (leftMax - height[left]);
+                left++;
+            }
         }
         
         return ans;
