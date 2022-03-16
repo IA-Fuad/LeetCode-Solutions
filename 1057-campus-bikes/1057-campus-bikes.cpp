cb3fe9d02dfed733b919452307b19ad7b921accb
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-03-16 11:19:44 +0600

    Time: 754 ms (29.84%), Space: 139.3 MB (6.36%) - LeetHub

--- 1057-campus-bikes/1057-campus-bikes.cpp
+++ 1057-campus-bikes/1057-campus-bikes.cpp
@@ -1,16 +1,4 @@
 class Solution {
-    struct DistanceInfo {
-        int wi;
-        int bi;
-        int dist;
-        
-        DistanceInfo(int wi, int bi, int dist) {
-            this->wi = wi;
-            this->bi = bi;
-            this->dist = dist;
-        }
-    };
-    
     int getManhattanDistance(vector<int> a, vector<int> b) {
         int xDist = abs(a[0] - b[0]);
         int yDist = abs(a[1] - b[1]);
@@ -19,41 +7,28 @@ class Solution {
 
 public:
     vector<int> assignBikes(vector<vector<int>>& workers, vector<vector<int>>& bikes) {
-        // auto sortComparer = [](const DistanceInfo& x, const DistanceInfo& y)->bool {
-        //     if (x.dist == y.dist) {
-        //         if (x.wi == y.wi) {
-        //             return x.bi > y.bi;
-        //         }
-        //         return x.wi > y.wi;
-        //     }
-        //     return x.dist > y.dist;
-        // };
-
-        //typedef priority_queue<DistanceInfo, vector<DistanceInfo>, decltype(sortComparer)> PQ;
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> Q;
-        
-        for (int b = 0; b < bikes.size(); b++) {
-            for (int w = 0; w < workers.size(); w++) {
-                Q.push({getManhattanDistance(bikes[b], workers[w]), b, w});
+        vector<vector<pair<int, int>>> distanceToPairs(2000);
+        for (int i = 0; i < workers.size(); i++) {
+            for (int j = 0; j < bikes.size(); j++) {
+                distanceToPairs[getManhattanDistance(workers[i], bikes[j])].push_back({i, j});
             }
         }
         
-        unordered_set<int> bikeTaken, workerTaken;
-        vector<int> ans(workers.size());
-        int k = 0;
-
-        while (k < workers.size()) {
-            int wi = get<2>(Q.top());
-            int bi = get<1>(Q.top());
-            Q.pop();
-            if (bikeTaken.find(bi) == bikeTaken.end() and workerTaken.find(wi) == workerTaken.end()) {
-                bikeTaken.insert(bi);
-                workerTaken.insert(wi);
-                ans[wi] = bi;
-                k++;
+        vector<int> assignedBikes(workers.size(), -1);
+        unordered_set<int> bikeTaken;
+        int pairCount = 0, d = 0;
+        
+        while (pairCount < workers.size()) {
+            for (auto[w, b] : distanceToPairs[d]) {
+                if (bikeTaken.find(b) == bikeTaken.end() and assignedBikes[w] == -1) {
+                    assignedBikes[w] = b;
+                    bikeTaken.insert(b);
+                    pairCount++;
+                }
             }
+            d++;
         }
         
-        return ans;
+        return assignedBikes;
     }
 };
