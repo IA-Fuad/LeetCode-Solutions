93f414642f70bcb60a76de08bfd40ab41e377404
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-03-17 19:07:14 +0600

    Time: 5 ms (14.59%), Space: 6.1 MB (45.05%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -2,32 +2,18 @@ class Solution {
 public:
     int scoreOfParentheses(string s) {
         stack<int> scores;
+        scores.push(0);
         for (char c : s) {
             if (c == '(') scores.push(0);
             else {
-                if (scores.top() == 0) {
-                    scores.pop();
-                    scores.push(1);
-                }
-                else {
-                    int currentScore = 0;
-                    while (scores.top() != 0) {
-                        currentScore += scores.top();
-                        scores.pop();
-                    }
-                    scores.pop();
-                    scores.push(2 * currentScore);
-                }
+                int prev = scores.top();
+                scores.pop();
+                int prevPrev = scores.top();
+                scores.pop();
+                scores.push(max(1, 2 * prev) + prevPrev);
             }
         }
-        
-        int totalScore = 0;
-        while (!scores.empty()) {
-            totalScore += scores.top();
-            scores.pop();
-        }
-        
-        return totalScore;
+        return scores.top();
     }
 };
 
@@ -35,8 +21,6 @@ public:
 
 (((()))())()()()
 
-[0,0,0,0]
-[0,0,0,1]
-[0,0,]
+[1]
 
 */
