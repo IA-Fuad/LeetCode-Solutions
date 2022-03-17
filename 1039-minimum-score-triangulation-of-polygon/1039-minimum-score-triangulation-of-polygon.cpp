b6f9f0d176a635b22868469e013ea6ed7f0781b4
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-03-18 01:53:04 +0600

    Time: 17 ms (22.63%), Space: 8.5 MB (39.27%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
@@ -25,8 +25,9 @@ public:
         
         for (int i = n-3; i >= 0; i--) {
             for (int j = i+2; j < n; j++) {
+                dp[i][j] = INT_MAX;
                 for (int k = i+1; k < j; k++) {
-                    dp[i][j] = min(dp[i][j] == 0 ? INT_MAX : dp[i][j], dp[i][k] + dp[k][j] + (values[i] * values[k] * values[j]));
+                    dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j] + (values[i] * values[k] * values[j]));
                 }
             }
         }
