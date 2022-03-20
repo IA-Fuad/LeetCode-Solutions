5bb285774a65f1e88e34e997a518a3b4d6ae86d3
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-03-21 01:18:43 +0600

    Time: 98 ms (49.27%), Space: 9.5 MB (92.59%) - LeetHub

--- 741-cherry-pickup/741-cherry-pickup.cpp
+++ 741-cherry-pickup/741-cherry-pickup.cpp
@@ -2,8 +2,9 @@ class Solution {
     int dp[51][51][51];
     bool isValid = false;
     
-    int rec(int i1, int j1, int i2, int j2, vector<vector<int>>& grid) {
+    int rec(int i1, int j1, int i2, vector<vector<int>>& grid) {
         int n = grid.size();
+        int j2 = (i1 + j1) - i2;
         if (max({i1, j1, i2, j2}) >= n or grid[i1][j1] == -1 or grid[i2][j2] == -1) {
             return INT_MIN;
         }
@@ -11,10 +12,10 @@ class Solution {
         if ((i1 == n-1 and j1 == n-1) and (i2 == n-1 and j2 == n-1)) return grid[i1][j1];
         if (dp[i1][j1][i2] != -1) return dp[i1][j1][i2];
         
-        int a = rec(i1+1, j1, i2+1, j2, grid);
-        int b = rec(i1+1, j1, i2, j2+1, grid);
-        int c = rec(i1, j1+1, i2+1, j2, grid);
-        int d = rec(i1, j1+1, i2, j2+1, grid);
+        int a = rec(i1+1, j1, i2+1, grid);
+        int b = rec(i1+1, j1, i2, grid);
+        int c = rec(i1, j1+1, i2+1, grid);
+        int d = rec(i1, j1+1, i2, grid);
 
         int mx = max({a, b, c, d});
         
@@ -26,6 +27,6 @@ class Solution {
 public:
     int cherryPickup(vector<vector<int>>& grid) {
         memset(dp, -1, sizeof dp);
-        return max(0, rec(0, 0, 0, 0, grid));
+        return max(0, rec(0, 0, 0, grid));
     }
 };
