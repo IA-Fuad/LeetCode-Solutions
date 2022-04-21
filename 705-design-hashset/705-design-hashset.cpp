9bb13456fa7952660d4910470f6fab368bb601bb
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-04-22 01:53:37 +0600

    Time: 103 ms (79.50%), Space: 43.9 MB (39.71%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -2,13 +2,13 @@ class MyHashSet {
 
     struct Node {
         int key;
-        Node* next;
-        Node* prev;
+        Node* left;
+        Node* right;
         Node* lastNode;
         
         Node(int key) {
             this->key = key;
-            next = prev = nullptr;
+            left = right = nullptr;
             lastNode = this;
         }
     };
@@ -16,12 +16,48 @@ class MyHashSet {
     vector<Node*> hashSet;
     const int size = 1000;
     
-    Node* getNodeByKey(int key) {
-        int index = key % size;
-        
-        auto curr = hashSet[index];
-        while (curr and curr->key != key) {
-            curr = curr->next;
+    Node* getNodeByKey(Node* root, int key) {
+        //cout << root->key << ' ' << key << endl;
+        if (!root or root->key == key) return root;
+        return key > root->key ? getNodeByKey(root->right, key) : getNodeByKey(root->left, key);
+    }
+    
+    int getInorderSuccessor(Node* root) {
+        root = root->right;
+        while (root->left) {
+            root = root->left;
+        }
+        return root->key;
+    }
+    
+    Node* deleteNodeFromBST(Node* curr, int key) {
+        if (!curr) {
+            return curr;
+        }
+        if (curr->key > key) {
+            curr->left = deleteNodeFromBST(curr->left, key);
+        }
+        else if (curr->key < key) {
+            curr->right = deleteNodeFromBST(curr->right, key);
+        }
+        else {
+            if (!curr->left and !curr->right) {
+                delete curr;
+                return nullptr;
+            }
+            if (!curr->right) {
+                auto temp = curr->left;
+                delete curr;
+                return temp;
+            }
+            else if (!curr->left) {
+                auto temp = curr->right;
+                delete curr;
+                return temp;
+            }
+            
+            curr->key = getInorderSuccessor(curr);
+            curr->right = deleteNodeFromBST(curr->right, curr->key);
         }
         return curr;
     }
@@ -32,32 +68,46 @@ public:
     }
     
     void add(int key) {
-        if (getNodeByKey(key)) {
+        //cout << "add " << key << endl;
+        if (contains(key)) {
             return;
         }
+        
         int index = key % size;
         auto newNode = new Node(key);
-        hashSet[index]->lastNode->next = newNode;
-        newNode->prev = hashSet[index]->lastNode;
-        hashSet[index]->lastNode = newNode;
+        auto root = hashSet[index];
+        
+        while (root) {
+            if (key > root->key) {
+                if (!root->right) {
+                    root->right = newNode;
+                    break;
+                }
+                else {
+                    root = root->right;
+                }
+            }
+            else {
+                if (!root->left) {
+                    root->left = newNode;
+                    break;
+                }
+                else {
+                    root = root->left;
+                }
+            }
+        }
     }
     
     void remove(int key) {
-        auto node = getNodeByKey(key);
-        if (node) {
-            node->prev->next = node->next;
-            if (node->next) {
-                node->next->prev = node->prev;
-            }
-            else {
-                hashSet[key%size]->lastNode = node->prev;
-            }
-            delete node;
-        }
+       // cout << "remove " << key << endl;
+        auto root = hashSet[key % size];
+        deleteNodeFromBST(root, key);
     }
     
     bool contains(int key) {
-        if (getNodeByKey(key)) return true;
+        //cout << "contains " << key << endl;
+        if (getNodeByKey(hashSet[key%size], key)) return true;
         return false;
     }
 };
