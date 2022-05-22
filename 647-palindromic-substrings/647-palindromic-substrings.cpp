c81107aad8d6ebcbe765352ec661a58336a16ecc
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-05-22 20:32:44 +0600

    Time: 4 ms (84.41%), Space: 6.1 MB (96.68%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -15,25 +15,8 @@ class Solution {
 public:
     int countSubstrings(string s) {
         int n = s.size();
-        //vector<bool> isPalindrome(n, false);
         int palindromeCount = 0;
- 
-        // for (int j = 0; j < n; j++) {
-        //     for (int i = 0; i <= j; i++) {
-        //         if (i == j) {
-        //             isPalindrome[i] = true;
-        //         }
-        //         else if (j == i+1) {
-        //             isPalindrome[i] = s[i] == s[j];
-        //         }
-        //         else {
-        //             isPalindrome[i] = isPalindrome[i+1] & s[i] == s[j];
-        //         }
-        //         if (isPalindrome[i]) {
-        //             palindromeCount++;
-        //         }
-        //     }
-        // }
+
         for (int i = 0; i < n; i++) {
             palindromeCount += getPalindromeCountAroundCenter(s, i, i);
             palindromeCount += getPalindromeCountAroundCenter(s, i, i+1);
