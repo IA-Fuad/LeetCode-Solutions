798357a1dc52d82d4e4b4c6180942c24bc41901b
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-06-05 20:02:17 +0600

    Time: 17 ms (26.74%), Space: 7.7 MB (22.71%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,12 +1,13 @@
 class Solution {
     typedef unordered_set<int> uiset;
-    int ans = 0;
-    
-    void rec(int n, int row, uiset& colFlag, uiset& diagFlag, uiset& antiDiagFlag) {
+
+    int rec(int n, int row, uiset& colFlag, uiset& diagFlag, uiset& antiDiagFlag) {
         if (row == n) {
-            ans++;
-            return;
+            return 1;
         }
+        
+        int ans = 0;
+        
         for (int col = 0; col < n; col++) {
             int diag = row - col, antiDiag = row + col;
             if (colFlag.find(col) == colFlag.end() and diagFlag.find(diag) == diagFlag.end() and antiDiagFlag.find(antiDiag) == antiDiagFlag.end()) {
@@ -14,19 +15,20 @@ class Solution {
                 diagFlag.insert(diag);
                 antiDiagFlag.insert(antiDiag);
                 
-                rec(n, row + 1, colFlag, diagFlag, antiDiagFlag);
+                ans += rec(n, row + 1, colFlag, diagFlag, antiDiagFlag);
                 
                 colFlag.erase(col);
                 diagFlag.erase(diag);
                 antiDiagFlag.erase(antiDiag);
             }
         }
+        
+        return ans;
     }
     
 public:
     int totalNQueens(int n) {
         uiset colFlag, diagFlag, antiDiagFlag;
-        rec(n, 0, colFlag, diagFlag, antiDiagFlag);
-        return ans;
+        return rec(n, 0, colFlag, diagFlag, antiDiagFlag);
     }
 };
