7f95f72f3a26521f24d54203721891ea5b9a8435
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-06-15 19:47:13 +0600

    Time: 145 ms (48.70%), Space: 13.2 MB (74.21%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,67 +1,34 @@
 class Solution {
-    bool isPredecessor(const string& a, const string& b) {
-        if (a.size() == b.size()) {
-            return false;
-        }
+    bool isPred(const string& pred, const string& word) {
+        if (pred.size() + 1 != word.size()) return false;
+        
         int i, j;
-        for (i = 0, j = 0; i < a.size() && j < b.size(); j++) {
-            if (a[i] == b[j]) {
-                i++;
-            }
-        }
-
-        return i == a.size();
-    }
-    
-    vector<int> dp;
-    
-    int rec(vector<string>& words, int i) {
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-        int mxLen = 1;
-        for (int j = i-1; j >= 0; j--) {
-            if (words[j].size() < words[i].size()-1) {
-                break;
-            }
-            if (isPredecessor(words[j], words[i])) {
-                mxLen = max(mxLen, rec(words, j) + 1);
-                //cout << words[j] << ' ' << words[i] << endl;
-            }
-        }
-        return dp[i] = mxLen;
+        for (i = 0, j = 0; i < pred.size() and j < word.size(); j++) {
+            if (pred[i] == word[j]) i++;
+        } 
+        
+        return (i == pred.size() and j >= word.size()-1);
     }
     
 public:
     int longestStrChain(vector<string>& words) {
-//         int n = words.size();
-//         sort(words.begin(), words.end(), cmp);
-//         vector<int> longestChains(n, 1);
-
-//         int longestChain = 1;
+        sort(words.begin(), words.end(), [](const string& x, const string& y) {
+            return x.size() < y.size();
+        });
         
-//         for (int i = 1; i < n; i++) {
-//             for (int j = i-1; j >= 0; j--) {
-//                 if (words[j].size() < words[i].size()-1) {
-//                     break;
-//                 }
-//                 if (isPredecessor(words[j], words[i]) && longestChains[j] + 1 > longestChains[i]) {
-//                     longestChains[i] = longestChains[j] + 1;
-//                 }
-//             }
-//             longestChain = max(longestChain, longestChains[i]);
-//         }
+        int n = words.size(), longestChain = 1;
+        vector<int> lis(n, 1);
         
-//         return longestChain;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+               // cout << words[j] << ' ' << words[i] << ' ' << isPred(words[j], words[i]) << endl;
+                if (isPred(words[j], words[i]) and lis[j] + 1 > lis[i]) {
+                    lis[i] = lis[j] + 1;
+                    longestChain = max(longestChain, lis[i]);
+                }
+            }
+        }
         
-        int n = words.size(), mxLen = 0;
-        dp.resize(n, -1);
-        sort(words.begin(), words.end(), cmp);
-        for (int i = n-1; i >= 0; i--) mxLen = max(mxLen, rec(words, i));
-        return mxLen;
-    }
-    
-    bool static cmp(string& x, string& y) {
-        return x.size() < y.size();
+        return longestChain;
     }
 };
