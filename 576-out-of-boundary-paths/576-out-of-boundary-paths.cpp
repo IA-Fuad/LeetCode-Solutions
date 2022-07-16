8ec6248c4ddd4b47764f6ad06b6aa9a84cd77773
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-07-17 00:26:05 +0600

    Time: 15 ms (57.00%), Space: 9.8 MB (22.78%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -1,50 +1,31 @@
 class Solution {
     const int mod = 1e9 + 7;
+    vector<vector<vector<int>>> dp;
     int dx[4] = {0, 0, 1, -1};
     int dy[4] = {1, -1, 0, 0};
     
-    int rec(vector<vector<vector<int>>>& dp, int i, int j, int m, int n, int move) {
-        if (i < 0 || j < 0 || i == m || j == n) return 1;
+    int rec(int m, int n, int move, int i, int j) {
+        if (i < 0 or j < 0 or i == m or j == n) return 1;
         if (move == 0) return 0;
-        if (dp[i][j][move] != -1) return dp[i][j][move];
+
+        int& ref = dp[i][j][move];
+        if (ref != -1) return ref;
         
         int paths = 0;
         for (int k = 0; k < 4; k++) {
-            int p = rec(dp, i+dx[k], j+dy[k], m, n, move-1);
-            paths = (paths + p) % mod;
+            paths = (paths + (rec(m, n, move-1, i+dx[k], j+dy[k]) % mod)) % mod;
         }
-        
-        return dp[i][j][move] = paths % mod;
+
+        return ref = paths;
     }
     
 public:
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        // vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(maxMove+1, -1)));
-        // return rec(dp, startRow, startColumn, m, n, maxMove);
-        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(maxMove+1, 0)));
-        
-        dp[startRow][startColumn][0] = 1;
-        int paths = 0;
-        
-        for (int move = 0; move < maxMove; move++) {
-            for (int i = 0; i < m; i++) {
-                for (int j = 0; j < n; j++) {
-                    for (int k = 0; k < 4; k++) {
-                        int x = i + dx[k];
-                        int y = j + dy[k];
-                        if (x < 0 || y < 0 || x == m || y == n) {
-                            paths += dp[i][j][move];
-                            paths %= mod;
-                        }
-                        else {
-                            dp[x][y][move+1] += dp[i][j][move];
-                            dp[x][y][move+1] %= mod;
-                        }
-                    }
-                }
-            }
+        dp.resize(m+1, vector<vector<int>>(n+1, vector<int>(maxMove+2, -1)));
+        for (int move = 1; move <= maxMove; move++) {
+            rec(m, n, move, startRow, startColumn);
         }
         
-        return paths;
+        return dp[startRow][startColumn][maxMove] == -1 ? 0 : dp[startRow][startColumn][maxMove];
     }
 };
