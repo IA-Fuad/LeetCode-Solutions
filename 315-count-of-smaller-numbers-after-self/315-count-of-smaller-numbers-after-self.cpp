a7d7c2270a9943ed911c1cd78721b43b94376dd7
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-07-23 20:11:58 +0600

    Time: 1630 ms (13.20%), Space: 469.6 MB (5.04%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -30,15 +30,50 @@ class Solution {
         return query(b, mid, l, r, x * 2) + query(mid+1, e, l, r, x * 2 + 1);
     }
     
+    void merge(vector<int>& cnt, vector<pair<int, int>>& nums, int l, int r) {
+        if (l >= r) return;
+        int mid = (l + r) / 2;
+        merge(cnt, nums, l, mid);
+        merge(cnt, nums, mid+1, r);
+        
+        vector<pair<int, int>> temp;
+        int fromRight = 0, i, j;
+        for (i = l, j = mid+1; i <= mid and j <= r; ) {
+            if (nums[i] <= nums[j]) {
+                cnt[nums[i].second] += fromRight;
+                temp.push_back(nums[i++]);
+            }
+            else {
+                temp.push_back(nums[j++]);
+                fromRight++;
+            }
+        }
+        while (i <= mid) {
+            cnt[nums[i].second] += fromRight;
+            temp.push_back(nums[i++]);
+        }
+        while (j <= r) {
+            temp.push_back(nums[j++]);
+        }
+        
+        for (int i = l, k = 0; i <= r; i++) nums[i] = temp[k++];
+    }
+    
 public:
     vector<int> countSmaller(vector<int>& nums) {
         int n = nums.size();
         vector<int> ans(n);
 
-        for (int i = n-1; i >= 0; i--) {
-            ans[i] = query(0, offset+offset, 0, nums[i]-1+offset, 1);
-            update(0, offset+offset, nums[i]+offset, 1, 1);
+//         for (int i = n-1; i >= 0; i--) {
+//             ans[i] = query(0, offset+offset, 0, nums[i]-1+offset, 1);
+//             update(0, offset+offset, nums[i]+offset, 1, 1);
+//         }
+        
+        vector<pair<int, int>> ni;
+        for (int i = 0; i < n; i++) {
+            ni.push_back({nums[i], i});
         }
+        merge(ans, ni, 0, n-1);
 
         return ans;
     }
