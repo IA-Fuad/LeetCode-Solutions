0927afd5a94537881369c3731dade0a0f7b1920e
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-08-12 23:07:17 +0600

    Time: 11 ms (6.55%), Space: 9.3 MB (5.98%) - LeetHub

---
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    unordered_map<TreeNode*, int> indegree;
+    unordered_map<TreeNode*, TreeNode*> parent;
+    queue<TreeNode*> leaves;
+    
+    void dfs(TreeNode* curr, TreeNode* p) {
+        if (!curr) return;
+        
+        parent[curr] = p;
+        if (p != nullptr) indegree[p]++;
+        
+        if (!curr->left and !curr->right) {
+            leaves.push(curr);
+            return;
+        }
+        
+        dfs(curr->left, curr);
+        dfs(curr->right, curr);
+    }
+    
+public:
+    vector<vector<int>> findLeaves(TreeNode* root) {
+        dfs(root, nullptr);
+        
+        vector<vector<int>> ans;
+        
+        while (!leaves.empty()) {
+            int currLeafCount = leaves.size();
+            vector<int> currentLeaves;
+            
+            while (--currLeafCount >= 0) {
+                auto leaf = leaves.front();
+                leaves.pop();
+                
+                currentLeaves.push_back(leaf->val);
+                
+                auto leafParent = parent[leaf];
+                if (--indegree[leafParent] == 0) {
+                    leaves.push(leafParent);
+                }
+            }
+            
+            ans.push_back(currentLeaves);
+        }
+        
+        return ans;
+    }
+};
