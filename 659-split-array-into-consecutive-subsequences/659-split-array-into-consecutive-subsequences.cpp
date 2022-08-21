010592c71b7532cfb04af2bba5994ca12f93b982
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-08-21 11:39:53 +0600

    Time: 109 ms (88.55%), Space: 54.5 MB (88.31%) - LeetHub

---
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -0,0 +1,59 @@
+class Solution {
+    typedef pair<int, int> pii;
+    
+public:
+    bool isPossible(vector<int>& nums) {
+        auto comp = [](pii& x, pii& y) {
+            if (x.first == y.first) return x.second > y.second;
+            return x.first > y.first;
+        };
+        
+        priority_queue<pii, vector<pii>, decltype(comp)> Q(comp);
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int num = i < nums.size() ? nums[i] : INT_MAX;
+            
+            if (Q.empty()) {
+                Q.push({num, 1});
+            }
+            else {
+                auto[n, len] = Q.top();
+               // cout << num << ' ' << n << ' ' << len << endl;
+                
+                if (num == n) {
+                    Q.push({num, 1});
+                }
+                else {
+                    while (!Q.empty()) {
+                    auto[n, len] = Q.top();
+                    Q.pop();
+
+                    if (num-1 == n) {
+                        Q.push({num, len+1});
+                        break;
+                    }
+                    else if (len < 3) {
+                        return false;
+                    }
+                    }
+                    if (Q.empty()) {
+                        Q.push({num, 1});
+                    }
+                }
+            }
+        }
+        
+        while (!Q.empty()) {
+            if (Q.top().second < 3) return false;
+            Q.pop();
+        }
+        
+        return true;
+    }
+};
+
+/*
+
+
+
+*/
