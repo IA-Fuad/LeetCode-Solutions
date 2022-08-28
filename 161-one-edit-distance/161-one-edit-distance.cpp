fbda666b74e1bc57072b94191a74766e489137f6
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2022-08-28 19:43:04 +0600

    Time: 0 ms (100.00%), Space: 6.6 MB (60.87%) - LeetHub

--- 161-one-edit-distance/161-one-edit-distance.cpp
+++ 161-one-edit-distance/161-one-edit-distance.cpp
@@ -3,30 +3,23 @@ public:
     bool isOneEditDistance(string s, string t) {
         int n = s.size(), m = t.size();
         
-        if (n == m) {
-            int misplacedCount = 0;
-            for (int i = 0; i < n; i++) {
-                if (s[i] != t[i]) {
-                    misplacedCount++;
-                    if (misplacedCount > 1) return false;
-                }
-            }
-            return misplacedCount == 1;
-        }
         if (abs(n - m) > 1) return false;
-        
         if (n < m) {
             swap(s, t);
             swap(n, m);
         }
         
-        for (int i = 0, j = 0; i < n; ) {
-            if (j == m and i == n-1) return true;
-            if (s[i] != t[j]) i++;
+        int misplacedCount = 0;
+        
+        for (int i = 0, j = 0; i < n and j < m; ) {
+            if (s[i] != t[j]) {
+                misplacedCount++;
+                if (n > m) i++;
+                else i++, j++;
+            }
             else i++, j++;
-            if (i - j > 1) return false;
         }
         
-        return true;
+        return misplacedCount == 1 or (misplacedCount == 0 and n > m);
     }
 };
