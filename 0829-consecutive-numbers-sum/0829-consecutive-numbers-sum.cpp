05f90e3cc806c436a181f6b7ea3259987539cdc7
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2023-07-09 14:12:59 +0100

    Time: 6 ms (47.14%), Space: 5.9 MB (90.95%) - LeetHub

--- 0829-consecutive-numbers-sum/0829-consecutive-numbers-sum.cpp
+++ 0829-consecutive-numbers-sum/0829-consecutive-numbers-sum.cpp
@@ -3,8 +3,8 @@ public:
     int consecutiveNumbersSum(int n) {
         int consCount = 0;
         
-        for (int k = 1; k*(k-1) < 2*n; k++) {
-            if ((n - k * (1 + k) / 2) % k == 0) {
+        for (int k = 1; k*(1+k) <= 2*n; k++) {
+            if ((n - (k * (1 + k)) / 2) % k == 0) {
                 consCount++;        
             }
         }
@@ -15,14 +15,14 @@ public:
 
 /*
 
-x, x+1, x+2, x+3, x+k-1
+x+1, x+2, x+3, x+k
 
-kx + k(k-1)/2 = n
-kx = n - k(k-1)/2
+kx + k(1 + k)/2 = n
+kx = n - k(1+k)/2
 
 
-n - k(k-1)/2 > 0
-n > k(k-1)/2
-2n > k(k-1)
+n - k(1+k)/2 > 0
+n > k(1+k)/2
+2n > k(1+k)
 
 */
