43ce86fb622b97f8c010c533cc47090be1f58d5a
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2023-08-12 16:42:59 +0100

    Time: 197 ms (94.58%), Space: 137 MB (32.85%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        priority_queue<pair<int, int>> Q;
+        deque<pair<int, int>> Q;
         int n = nums.size();
         vector<int> ans;
         
-        for (int i = 0; i < k-1; i++) Q.push({nums[i], i});
-        
-        for (int i = k-1; i < n; i++) {
-            Q.push({nums[i], i});
+        for (int i = 0; i < n; i++) {
+            while (!Q.empty() and Q.back().first < nums[i]) {
+                Q.pop_back();
+            }
+            Q.push_back({nums[i], i});
             int leftBound = i - k + 1;
             
-            while (Q.top().second < leftBound) {
-                Q.pop();    
-            }
-            ans.push_back(Q.top().first);
+            if (Q.front().second < leftBound) Q.pop_front();
+            
+            if (leftBound >= 0) ans.push_back(Q.front().first);
         }
         
         return ans;
     }
 };
+
+/*
+
+7 5 1 4 3 9
+
+5 4
+
+4 3
+
+9
+
+*/
