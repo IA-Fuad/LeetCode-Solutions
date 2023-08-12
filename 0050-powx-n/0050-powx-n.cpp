e22ebab2ac5d0c0263a84a22ed64cf5a477218e6
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2023-08-12 21:50:20 +0100

    Time: 0 ms (100.00%), Space: 6.6 MB (6.88%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,16 @@
 class Solution {
+    unordered_map<int, double> p;
+    
 public:
     double myPow(double x, int n) {
-        return pow(x, n);
+        if (n == 0) return 1;
+        if (n == 1) return x;
+        if (n == -1) return (1.0 / x);
+        if (p.find(n) != p.end()) return p[n];
+        
+        int a = n / 2;
+        int b = (n&1) ? ((n < 0) ? a - 1 : a + 1) : a;
+        
+        return p[n] = myPow(x, a) * myPow(x, b);
     }
 };
