2688d1a4644f083d40aba44027c7fbd30244d260
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2023-08-13 01:42:41 +0100

    Time: 97 ms (90.12%), Space: 70.5 MB (56.98%) - LeetHub

--- 1771-maximize-palindrome-length-from-subsequences/1771-maximize-palindrome-length-from-subsequences.cpp
+++ 1771-maximize-palindrome-length-from-subsequences/1771-maximize-palindrome-length-from-subsequences.cpp
@@ -3,6 +3,7 @@ public:
     int longestPalindrome(string word1, string word2) {
         string word = word1 + word2;
         int N = word.size();
+        int n = word1.size(), m = word2.size(), maxLen = 0;
         
         vector<vector<int>> dp(N, vector<int>(N));
         
@@ -12,6 +13,7 @@ public:
             for (int i = j-1; i >= 0; i--) {
                 if (word[i] == word[j]) {
                     dp[i][j] = dp[i+1][j-1] + 2;
+                    if (i < n and j >= n) maxLen = max(maxLen, dp[i][j]);
                 }
                 else {
                     dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
@@ -20,14 +22,14 @@ public:
             }
         }
         //cout << word << ' ' << dp[0][N-1] << endl;
-        int n = word1.size(), m = word2.size(), maxLen = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (word1[i] == word2[j]) {
-                    maxLen = max(maxLen, dp[i][j+n]);
-                }
-            }
-        }
+        
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < m; j++) {
+//                 if (word1[i] == word2[j]) {
+//                     maxLen = max(maxLen, dp[i][j+n]);
+//                 }
+//             }
+//         }
         
         return maxLen;
     }
