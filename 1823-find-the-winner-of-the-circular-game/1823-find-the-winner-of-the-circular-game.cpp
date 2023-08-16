cb1c294cc9cc23cd10b8a3e97bc5abbaa057a502
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2023-08-16 01:17:03 +0100

    Time: 0 ms (100.00%), Space: 6 MB (63.71%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,8 +1,22 @@
-class Solution { 
+class Solution {
+    int rec(int n, int k) {
+        if (n == 1) return 0;
+        return (rec(n-1, k) + k) % n;
+    }
+    
 public:
     int findTheWinner(int n, int k) {
-        if (n == 1) return 1;
-        
-        return (findTheWinner(n - 1, k) + k - 1) % n + 1;
+        int x = rec(n, k);
+        return x + 1;
     }
 };
+
+
+/*
+
+1 2 3 4 5
+    1 2 3 4
+        1 2 3
+            1
+
+*/
