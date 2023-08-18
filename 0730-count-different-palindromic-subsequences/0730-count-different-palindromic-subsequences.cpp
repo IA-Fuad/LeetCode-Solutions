f6f6431eb831918faba09d0212976cb59c16e6e7
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2023-08-18 16:11:02 +0100

    Time: 91 ms (84.14%), Space: 66 MB (28.73%) - LeetHub

--- 0730-count-different-palindromic-subsequences/0730-count-different-palindromic-subsequences.cpp
+++ 0730-count-different-palindromic-subsequences/0730-count-different-palindromic-subsequences.cpp
@@ -1,36 +1,34 @@
 class Solution {
-    const int64_t mod = 1e9 + 7;
-    
 public:
     int countPalindromicSubsequences(string s) {
+        const int mod = 1e9 + 7;
         int n = s.size();
         vector<vector<int64_t>> dp(n, vector<int64_t>(n, 0));
         
         for (int j = 0; j < n; j++) {
             dp[j][j] = 1;
+            
             for (int i = j-1; i >= 0; i--) {
                 if (s[i] != s[j]) {
-                    dp[i][j] = dp[i+1][j] + dp[i][j-1] - dp[i+1][j-1];
+                    dp[i][j] = (dp[i+1][j] + dp[i][j-1] - dp[i+1][j-1]);
                 }
                 else {
                     dp[i][j] = (dp[i+1][j-1] * 2);
                     
-                    int left = i + 1, right = j - 1;
+                    int left = i+1, right = j-1;
                     
-                    while (left <= right and s[left] != s[i]) left++;
-                    while (right >= left and s[right] != s[i]) right--;
+                    while (left < n and s[left] != s[i]) left++;
+                    while (right >= 0 and s[right] != s[j]) right--;
                     
-                    if (left == right) dp[i][j]++;
-                    else if (left > right) dp[i][j] += 2;
-                    else {
-                        dp[i][j] -= dp[left+1][right-1];
-                    }
+                    if (left > right) dp[i][j] += 2;
+                    else if (left == right) dp[i][j] += 1;
+                    else dp[i][j] -= dp[left+1][right-1];
                 }
-                if (dp[i][j] < 0) dp[i][j] += mod;
-                dp[i][j] %= mod;
+                dp[i][j] = (dp[i][j] + mod) % mod;
+                //cout << i << ' ' << j << ' ' << dp[i][j] << endl;
             }
         }
         
-        return dp[0][n-1];// % mod;
+        return dp[0][n-1] % mod;
     }
 };
