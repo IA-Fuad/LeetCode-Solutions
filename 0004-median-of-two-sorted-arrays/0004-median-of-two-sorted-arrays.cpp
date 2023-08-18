e1992b73e1ad5d72dd794a76c20893d1cdae02cc
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2023-08-18 15:51:19 +0100

    Time: 15 ms (98.63%), Space: 89.2 MB (75.30%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,24 +1,28 @@
 class Solution {
-    double getMedian(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size(), m = nums2.size();
-        int mi = (n + m + 1) / 2;
-        int left = 0, right = n;
-        bool isOdd = (n + m) & 1;
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        
+        int m = nums1.size(), n = nums2.size();
+        int medianInd = (m + n + 1) / 2;
+        int left = 0, right = m;
+        bool isOdd = (m + n) & 1;
         
         while (left <= right) {
             int x = left + (right - left) / 2;
-            int y = mi - x;
-            //cout << isOdd << ' ' << mi << ' ' << x << ' ' << y << endl;
+            int y = medianInd - x;
             
-            int minA = x == 0 ? INT_MIN : nums1[x-1];
-            int minB = y == 0 ? INT_MIN : nums2[y-1];
-            int maxA = x >= n ? INT_MAX : nums1[x];
-            int maxB = y >= m ? INT_MAX : nums2[y];
+            int leftMaxA = x == 0 ? INT_MIN : nums1[x-1];
+            int rightMinA = x == m ? INT_MAX : nums1[x];
+            int leftMaxB = y == 0 ? INT_MIN : nums2[y-1];
+            int rightMinB = y == n ? INT_MAX : nums2[y];
             
-            if (minA <= maxB and minB <= maxA) {
-                return isOdd ? max(minA, minB) : (max(minA, minB) + min(maxA, maxB)) / 2.0;
+            if (leftMaxA <= rightMinB and leftMaxB <= rightMinA) {
+                return isOdd ? max(leftMaxA, leftMaxB) : (max(leftMaxA, leftMaxB) + min(rightMinA, rightMinB)) / 2.0;
             }
-            if (minA > maxB) {
+            if (leftMaxA > rightMinB) {
                 right = x - 1;
             }
             else {
@@ -27,26 +31,5 @@ class Solution {
         }
         
         return -1;
-    } 
-    
-public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if (nums1.size() < nums2.size()) return getMedian(nums1, nums2);
-        return getMedian(nums2, nums1);
     }
 };
-
-/*
-
-2 5 8
-1 3 7
-
-0 - 2
-3 - 5
-
-0 - 4
-5 - 5
-
-
-
-*/
