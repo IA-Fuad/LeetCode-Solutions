76c6481df88cf4900b25421c0945373a0b5fe8a8
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2023-08-20 15:38:17 +0100

    Time: 3 ms (39.08%), Space: 6.6 MB (46.91%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,60 +1,31 @@
 class Solution {
-    bool isDigit(char c) {
-        return c >= '0' and c <= '9';
-    }
+    int k = 0;
     
 public:
     string decodeString(string s) {
-        stack<string> S;
-        string curr;
-
-        for (int i = 0; i <= s.size(); i++) {
-            char c = i == s.size() ? ']' : s[i];
-          //  cout << c << ' ';
-            if (c == '[') {
-                S.push(curr);
-                curr.clear();
-            }
-            else if (c == ']') {
-                string str = "";
-                while (!S.empty() and !isDigit(S.top()[0])) {
-                    str = S.top() + str;
-                    S.pop();
+        string decode;
+        while (k < s.size()) {
+            if (!isdigit(s[k])) {
+                if (s[k] == ']') {
+                    k++;
+                    break;
                 }
-                if (curr.size() > 0) {
-                    str += curr;
-                    curr.clear();
-                }
-
-               // cout << S.size() << ' ' << S.top() << ' ';
-                
-                int repeat;
-                if (S.empty() or !isDigit(S.top()[0])) repeat = 1;
-                else {
-                    repeat = stoi(S.top());
-                    S.pop();
-                }
-
-                string decode = str;
-                while (--repeat > 0) {
-                    decode += str;
-                }
-
-                S.push(decode);
-               // cout << decode << endl;
+                decode.push_back(s[k++]);
             }
             else {
-                if (curr.size() == 0) curr.push_back(c);
-                else if (isDigit(c) ^ isDigit(curr[0])) {
-                    S.push(curr);
-                    curr = c;
+                string digit;
+                while (s[k] != '[') {
+                    digit.push_back(s[k++]);
                 }
-                else {
-                    curr.push_back(c);
+                k++;
+                
+                int repeat = stoi(digit);
+                string ds = decodeString(s);
+                while (--repeat >= 0) {
+                    decode += ds;
                 }
             }
         }
-
-        return S.top();
+        return decode;
     }
 };
