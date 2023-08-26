4cc4f2db55095ce93f1a1c325bf2435839180709
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2023-08-26 14:04:18 +0100

    Time: 3 ms (78.55%), Space: 6.6 MB (78.79%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,23 +1,46 @@
 class Solution {
-    vector<vector<vector<int>>> dp;
+//     vector<vector<vector<int>>> dp;
     
-    bool rec(const string& s1, const string& s2, const string& s3, int i, int j, int k) {
-        if (k == s3.size()) return true;
-        if (dp[k][i][j] != -1) return dp[k][i][j];
+//     bool rec(const string& s1, const string& s2, const string& s3, int i, int j, int k) {
+//         if (k == s3.size()) return true;
+//         if (dp[k][i][j] != -1) return dp[k][i][j];
         
-        bool match = false;
-        if (i < s1.size() and s1[i] == s3[k]) match = rec(s1, s2, s3, i + 1, j, k + 1);
-        if (j < s2.size() and s2[j] == s3[k]) match |= rec(s1, s2, s3, i, j + 1, k + 1);
+//         bool match = false;
+//         if (i < s1.size() and s1[i] == s3[k]) match = rec(s1, s2, s3, i + 1, j, k + 1);
+//         if (j < s2.size() and s2[j] == s3[k]) match |= rec(s1, s2, s3, i, j + 1, k + 1);
         
-        return dp[k][i][j] = match;
-    }
+//         return dp[k][i][j] = match;
+//     }
     
 public:
     bool isInterleave(string s1, string s2, string s3) {
         int n = s1.size(), m = s2.size(), nm = s3.size();
         if (n + m != nm) return false;
         
-        dp.resize(nm, vector<vector<int>>(n+1, vector<int>(m+1, -1)));
-        return rec(s1, s2, s3, 0, 0, 0);
+//         dp.resize(nm, vector<vector<int>>(n+1, vector<int>(m+1, -1)));
+//         return rec(s1, s2, s3, 0, 0, 0);
+        
+        vector<vector<bool>> dp(n+1, vector<bool>(m+1));
+        
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (i == 0 and j == 0) {
+                    dp[i][j] = true;
+                    continue;
+                }
+                int k = i + j - 1;
+                if (i == 0) {
+                    dp[i][j] = dp[i][j-1] & (s2[j-1] == s3[k]);
+                }
+                else if (j == 0) {
+                    dp[i][j] = dp[i-1][j] & (s1[i-1] == s3[k]);
+                }
+                else {
+                    dp[i][j] = (dp[i-1][j] && s1[i-1] == s3[k]) || (dp[i][j-1] && s2[j-1] == s3[k]);
+                }
+            }
+        }
+        
+        return dp[n][m];
     }
 };
