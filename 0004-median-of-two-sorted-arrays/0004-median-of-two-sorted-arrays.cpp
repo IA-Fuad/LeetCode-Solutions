acf44b5fe852dbaf02a7f5e607d7663db17c9fee
Ishtiak Ahmed Fuad <ishtiakahmedfuad2@gmail.com>
2023-09-21 21:05:16 +0100

    Time: 27 ms (58.49%), Space: 89.6 MB (47.70%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if (nums1.size() > nums2.size()) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
+        if (nums1.size() > nums2.size()) return findMedianSortedArrays(nums2, nums1);
         
-        int m = nums1.size(), n = nums2.size();
-        int medianInd = (m + n + 1) / 2;
-        int left = 0, right = m;
-        bool isOdd = (m + n) & 1;
+        int n = nums1.size(), m = nums2.size();
+        int left = 0, right = n, medianInd = (n + m + 1) / 2;
         
         while (left <= right) {
-            int x = left + (right - left) / 2;
+            int x = (left + right) / 2;
             int y = medianInd - x;
             
-            int leftMaxA = x == 0 ? INT_MIN : nums1[x-1];
-            int rightMinA = x == m ? INT_MAX : nums1[x];
-            int leftMaxB = y == 0 ? INT_MIN : nums2[y-1];
-            int rightMinB = y == n ? INT_MAX : nums2[y];
+            double leftMaxA = x == 0 ? INT_MIN : nums1[x-1];
+            double rightMinA = x == n ? INT_MAX : nums1[x];
+            double leftMaxB = y == 0 ? INT_MIN : nums2[y-1];
+            double rightMinB = y == m ? INT_MAX : nums2[y];
             
             if (leftMaxA <= rightMinB and leftMaxB <= rightMinA) {
-                return isOdd ? max(leftMaxA, leftMaxB) : (max(leftMaxA, leftMaxB) + min(rightMinA, rightMinB)) / 2.0;
+                return ((n + m) & 1) ? max(leftMaxA, leftMaxB) : (max(leftMaxA, leftMaxB) + min(rightMinA, rightMinB)) / 2.0;
             }
             if (leftMaxA > rightMinB) {
                 right = x - 1;
@@ -30,6 +26,6 @@ public:
             }
         }
         
-        return -1;
+        return 0;
     }
 };
